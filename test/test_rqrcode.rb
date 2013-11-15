07827d24dac09c87d9cbee9c3fe81b32ff89fe75
Adan Alvarado <adan.alvarado7@gmail.com>
2013-11-15 14:50:21 -0800

    Add as_blocks test

--- test/test_rqrcode.rb
+++ test/test_rqrcode.rb
@@ -87,11 +87,11 @@ class QRCodeTest < Test::Unit::TestCase
                  qr.to_s( :true => 'q', :false => 'n' )[0..21]
     assert_equal "@@@@@@@ @@ @  @@@@@@@\n", qr.to_s( :true => '@' )[0..21]
   end
-  
+
   def test_auto_alphanumeric
     # Overflowws without the alpha version
     assert RQRCode::QRCode.new( '1234567890', :size => 1, :level => :h )
-    
+
     qr = RQRCode::QRCode.new( 'DUNCAN', :size => 1, :level => :h )
     assert_equal "xxxxxxx xxx   xxxxxxx\n", qr.to_s[0..21]
   end
